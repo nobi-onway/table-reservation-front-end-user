6a4a18e2f16a2b566ae435ee68d73498ed643f51
Kim Loan <kimlloan146@gmail.com>
2023-07-19 00:47:39 +0700

    update table form submit reservation

--- src/pages/Reservation/components/TimePickerValue/index.jsx
+++ src/pages/Reservation/components/TimePickerValue/index.jsx
@@ -1,12 +1,19 @@
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs from 'dayjs';
 
-export default function TimePickerValue({ label, value, onChange }) {
+export default function TimePickerValue({ label, value, onChange, minutesStep, minTime, maxTime }) {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <TimePicker
                 sx={{ width: '100%' }}
+                disablePast
+                closeOnSelect
+                ampm={false}
+                minutesStep={minutesStep}
+                minTime={minTime}
+                maxTime={maxTime}
                 label={label}
                 value={value}
                 onChange={onChange}
